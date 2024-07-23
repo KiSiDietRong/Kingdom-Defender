973a24fe10e0802383017eb5feeef511f56ba55b
KiSiDietRong <hnguyendac942@gmail.com>
2024-07-23 20:20:25 +0700

    Music

---
+++ Assets/Scripts/AudioManage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManage : MonoBehaviour
+{
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, sfxSource;
+
+}
