df96c4a4acfe6adcf15e4601f908cec321841eb7
KiSiDietRong <hnguyendac942@gmail.com>
2024-07-24 10:12:07 +0700

    SettingandMusic

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider _musicSlider, _sfxSlider;
+
+    public void ToggleMusic()
     {
-        
+        AudioManage.Instance.ToggleMusic();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleSFX()
     {
-        
+        AudioManage.Instance.ToggleSFX();
+    }
+
+    public void MusicVolumn()
+    {
+        AudioManage.Instance.MusicVolumn(_musicSlider.value);
+    }
+
+    public void SFXVolumn()
+    {
+        AudioManage.Instance.SFXVolumn(_sfxSlider.value);
     }
 }
