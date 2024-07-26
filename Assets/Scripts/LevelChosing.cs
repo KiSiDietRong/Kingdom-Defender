85e526ce3e089aec2a3fe220ecb3c697874d9e3c
KiSiDietRong <hnguyendac942@gmail.com>
2024-07-26 09:57:54 +0700

    LoadingProgress

--- Assets/Scripts/LevelChosing.cs
+++ Assets/Scripts/LevelChosing.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,33 +6,60 @@ using UnityEngine.UI;
 public class LevelChosing : MonoBehaviour
 {
     public Button[] buttons;
-
     public GameObject levelsButton;
-
+    public GameObject loadingPanel; 
+    public Slider progressBar; 
     private void Awake()
     {
         ButtonToArray();
         int unlockLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
-        for(int i = 0; i < buttons.Length; i++)
+        for (int i = 0; i < buttons.Length; i++)
         {
             buttons[i].interactable = false;
         }
-        for(int i = 0; i < unlockLevel; i++)
+        for (int i = 0; i < unlockLevel; i++)
         {
             buttons[i].interactable = true;
         }
     }
+
     public void OpenLevel(int lvlId)
     {
         string levelName = "Level " + lvlId;
-        SceneManager.LoadScene(levelName);
+        StartCoroutine(LoadLevelAsync(levelName));
+    }
+
+    private IEnumerator LoadLevelAsync(string levelName)
+    {
+    
+        loadingPanel.SetActive(true);
+
+  
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelName);
+
+        
+        progressBar.value = 0;
+        progressBar.maxValue = 1;
+
+        
+        while (!asyncLoad.isDone)
+        {
+           
+            progressBar.value = asyncLoad.progress;
+
+            
+            yield return null;
+        }
+
+        
+        loadingPanel.SetActive(false);
     }
 
     void ButtonToArray()
     {
         int count = levelsButton.transform.childCount;
         buttons = new Button[count];
-        for(int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++)
         {
             buttons[i] = levelsButton.transform.GetChild(i).GetComponent<Button>();
         }
